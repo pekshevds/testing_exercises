e31460044392b39276a438c36589ad86723e22f5
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-09 12:49:53 +0300

    correct mistakes in test_four_bank_parser module

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -8,7 +8,7 @@ from functions.level_1.four_bank_parser import (
     parse_ineco_expense,
 )
 
-date_format = "%d.%m.%Y %H:%M"
+date_format = "%d.%m.%y %H:%M"
 
 bank_card1 = BankCard(last_digits="4321", owner="denis pekshev")
 bank_card2 = BankCard(last_digits="4789", owner="john smith")
@@ -20,30 +20,30 @@ bank_card3 = BankCard(last_digits="7654", owner="sarah connor")
     [
         (
             SmsMessage(
-                text="10000 99, 9874-2590-3658-4321 01.01.24 08:35 spent_in authcode 9874",
+                text="10000 99, 9874-2590-3658-4321 31.10.24 18:00 spent_in authcode 9874",
                 author="denis pekshev",
-                sent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+                sent_at=datetime.datetime.strptime("31.10.24 18:00", date_format),
             ),
             [bank_card1, bank_card2, bank_card3],
             Expense(
                 amount=Decimal("10000"),
                 card=bank_card1,
                 spent_in="spent_in",
-                spent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+                spent_at=datetime.datetime.strptime("31.10.24 18:00", date_format),
             ),
         ),
         (
             SmsMessage(
-                text="5 00, 9874-2590-3658-7654 01.01.24 08:35 spent_in authcode 9874",
+                text="5 00, 9874-2590-3658-7654 31.10.24 18:00 spent_in authcode 9874",
                 author="sarah connor",
-                sent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+                sent_at=datetime.datetime.strptime("31.10.24 18:00", date_format),
             ),
             [bank_card1, bank_card2, bank_card3],
             Expense(
                 amount=Decimal("5"),
                 card=bank_card3,
                 spent_in="spent_in",
-                spent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+                spent_at=datetime.datetime.strptime("31.10.24 18:00", date_format),
             ),
         ),
     ],
