878b5cfc8ad2a9616f98c0cbeee772e8fcd20dd4
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-09 11:48:18 +0300

    test_four_bank_parser.py

--- functions/level_1/four_bank_parser.py
+++ functions/level_1/four_bank_parser.py
@@ -22,12 +22,12 @@ class Expense(NamedTuple):
 
 
 def parse_ineco_expense(sms: SmsMessage, cards: list[BankCard]) -> Expense:
-    raw_sum, raw_details = sms.text.split(', ')
-    raw_details = raw_details.split(' authcode ')[0]
-    raw_card, raw_date, raw_time, spend_in = raw_details.split(' ', maxsplit=3)
+    raw_sum, raw_details = sms.text.split(", ")
+    raw_details = raw_details.split(" authcode ")[0]
+    raw_card, raw_date, raw_time, spend_in = raw_details.split(" ", maxsplit=3)
     return Expense(
-        amount=decimal.Decimal(raw_sum.split(' ')[-2]),
+        amount=decimal.Decimal(raw_sum.split(" ")[-2]),
         card=[c for c in cards if c.last_digits == raw_card[-4:]][0],
         spent_in=spend_in,
-        spent_at=datetime.datetime.strptime(f'{raw_date} {raw_time}', '%d.%m.%y %H:%M'),
+        spent_at=datetime.datetime.strptime(f"{raw_date} {raw_time}", "%d.%m.%y %H:%M"),
     )
--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,5 +1,52 @@
-from functions.level_1.four_bank_parser import BankCard, SmsMessage, Expense, parse_ineco_expense
+import datetime
+from decimal import Decimal
+import pytest
+from functions.level_1.four_bank_parser import (
+    BankCard,
+    SmsMessage,
+    Expense,
+    parse_ineco_expense,
+)
+
+date_format = "%d.%m.%Y %H:%M"
+
+bank_card1 = BankCard(last_digits="4321", owner="denis pekshev")
+bank_card2 = BankCard(last_digits="4789", owner="john smith")
+bank_card3 = BankCard(last_digits="7654", owner="sarah connor")
 
 
-def test_parse_ineco_expense():
-    pass
+@pytest.mark.parametrize(
+    "sms,cards,expected",
+    [
+        (
+            SmsMessage(
+                text="10000 99, 9874-2590-3658-4321 01.01.24 08:35 spent_in authcode 9874",
+                author="denis pekshev",
+                sent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+            ),
+            [bank_card1, bank_card2, bank_card3],
+            Expense(
+                amount=Decimal("10000"),
+                card=bank_card1,
+                spent_in="spent_in",
+                spent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+            ),
+        ),
+        (
+            SmsMessage(
+                text="5 00, 9874-2590-3658-7654 01.01.24 08:35 spent_in authcode 9874",
+                author="sarah connor",
+                sent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+            ),
+            [bank_card1, bank_card2, bank_card3],
+            Expense(
+                amount=Decimal("5"),
+                card=bank_card3,
+                spent_in="spent_in",
+                spent_at=datetime.datetime.strptime("31.10.2024 18:00", date_format),
+            ),
+        ),
+    ],
+)
+def test_parse_ineco_expense(sms: SmsMessage, cards: list[BankCard], expected: Expense):
+    assert parse_ineco_expense(sms, cards) == expected
