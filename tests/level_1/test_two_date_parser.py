fec6af010f4e534e7a0ad6d0b3638ada6a382e36
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-11 14:34:21 +0300

    fix the bugs mentioned in the review

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,46 +1,60 @@
 import pytest
-import datetime
+from datetime import datetime, date, timedelta
 from functions.level_1.two_date_parser import compose_datetime_from
+
+# Сегодня строкой
+today_str: str = datetime.strftime(date.today(), "%d-%m-%Y")
+# Завтра строкой
+tomorrow_str: str = datetime.strftime(date.today() + timedelta(days=1), "%d-%m-%Y")
+# Текущий формат даты
+carrent_date_format: str = "%d-%m-%Y %H:%M"
 
 
 @pytest.mark.parametrize(
     "date_str,time_str,expected",
     [
         (
-            datetime.datetime.strftime(datetime.date.today(), "%d-%m-%Y"),  # date_str
+            today_str,  # date_str
             "08:35",  # time_str
-            datetime.datetime.strptime(
-                f"{datetime.datetime.strftime(datetime.date.today(), "%d-%m-%Y")} 08:35",
-                "%d-%m-%Y %H:%M",
-            ),  # expected
+            datetime.strptime(f"{today_str} 08:35", carrent_date_format),  # expected
         ),
         (
-            datetime.datetime.strftime(datetime.date.today(), "%d-%m-%Y"),  # date_str
+            today_str,  # date_str
             "23:59",  # time_str
-            datetime.datetime.strptime(
-                f"{datetime.datetime.strftime(datetime.date.today(), "%d-%m-%Y")} 23:59",
-                "%d-%m-%Y %H:%M",
+            datetime.strptime(
+                f"{today_str} 23:59",
+                carrent_date_format,
             ),  # expected
         ),
         (
-            datetime.datetime.strftime(datetime.date.today(), "%d-%m-%Y"),  # date_str
+            today_str,  # date_str
             "00:01",  # time_str
-            datetime.datetime.strptime(
-                f"{datetime.datetime.strftime(datetime.date.today(), "%d-%m-%Y")} 00:01",
-                "%d-%m-%Y %H:%M",
+            datetime.strptime(
+                f"{today_str} 00:01",
+                carrent_date_format,
             ),  # expected
         ),
         (
             "tomorrow",  # date_str
             "00:01",  # time_str
-            datetime.datetime.strptime(
-                f"{datetime.datetime.strftime(datetime.date.today() + datetime.timedelta(days=1), "%d-%m-%Y")} 00:01",
-                "%d-%m-%Y %H:%M",
+            datetime.strptime(
+                f"{tomorrow_str} 00:01",
+                carrent_date_format,
             ),  # expected
         ),
     ],
 )
 def test_compose_datetime_from(
-    date_str: str, time_str: str, expected: datetime.datetime
-):
+    date_str: str, time_str: str, expected: datetime
+) -> None:
     assert compose_datetime_from(date_str, time_str) == expected
+
+
+def test_compose_datetime_from_if_today() -> None:
+    expected: datetime = datetime.strptime(f"{today_str} 08:35", carrent_date_format)
+    assert compose_datetime_from(today_str, "08:35") == expected
+
+
+def test_compose_datetime_from_if_tomorrow() -> None:
+    expected: datetime = datetime.strptime(f"{tomorrow_str} 00:01", carrent_date_format)
+    assert compose_datetime_from("tomorrow", "00:01") == expected
